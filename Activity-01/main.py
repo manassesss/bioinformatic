a500a3c424d623f57a42c26ac286ee100c1b7419
manasses.silva <manasses3mm@gmail.com>
2021-09-06 10:00:18 -0300

    Activity-01: Transcribing DNA to RNA - two ways

--- Activity-01/main.py
+++ Activity-01/main.py
@@ -6,5 +6,9 @@ for i in input:
         output += 'U'
     else:
         output += i
-    
+
+f = open("rna.txt", "a")
+f.write(output)
+f.close()
+
 print("INPUT: ", input, "\nOUTPUT: ", output)
---
+++ Activity-01/main2.py
@@ -0,0 +1,33 @@
+dna = "TTCCAGGCGTCATCTCCTATATTCTGGGTCCTCTGGGGTCTGGGTTTGCCTCTGTGTAGTCACGTACATTGACTTTTCATCTGTGATCGCGCAATGAACTTTGTACTACCGACACTCAAGTAGCACACTTGTACAGGAAACAAGTCGGGT"
+mold = ""
+rna = ""
+
+# doing the mold to transcribe
+for protain in dna:
+    if protain == "T":
+        mold += "A"
+    elif protain == "A":
+        mold += "T"
+    elif protain == "G":
+        mold += "C"
+    elif protain == "C":
+        mold += "G"
+
+# transcribing
+for protain in range(len(dna)):
+    if dna[protain] == "A" and mold[protain] == "T":
+        rna += "A"
+    if dna[protain] == "T" and mold[protain] == "A":
+        rna += "U"
+    if dna[protain] == "C" and mold[protain] == "G":
+        rna += "C"
+    if dna[protain] == "G" and mold[protain] == "C":
+        rna += "G"
+
+#saving the result in a file
+f = open("rna.txt", "a")
+f.write(rna)
+f.close()
+
+
+print("INPUT: ", dna, "\nOUTPUT: ", rna)
